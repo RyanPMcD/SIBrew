9565b9a71bdff93c56c6693cacd3a3df297c5b2f
David Ohayon <david@evertrue.com>
2013-11-20 14:14:19 -0500

    Initial Commit

---
+++ SIBrew/SIBViewController.h
@@ -0,0 +1,13 @@
+//
+//  SIBViewController.h
+//  SIBrew
+//
+//  Created by David Ohayon on 11/20/13.
+//  Copyright (c) 2013 Startup Institute. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SIBViewController : UIViewController
+
+@end
---
+++ SIBrew/SIBViewController.m
@@ -0,0 +1,29 @@
+//
+//  SIBViewController.m
+//  SIBrew
+//
+//  Created by David Ohayon on 11/20/13.
+//  Copyright (c) 2013 Startup Institute. All rights reserved.
+//
+
+#import "SIBViewController.h"
+
+@interface SIBViewController ()
+
+@end
+
+@implementation SIBViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
